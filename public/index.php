bb2dbcf086c136e1c697b7d3bfa814a1c9032193
jsebast1an <juanselopezg05@gmail.com>
2026-03-27 00:42:55 -0500

    index.php fix 2

--- public/index.php
+++ public/index.php
@@ -6,15 +6,20 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+// Support both standard Laravel structure and shared hosting (flat) structure
+$basePath = file_exists(__DIR__.'/../vendor/autoload.php')
+    ? __DIR__.'/..'
+    : __DIR__;
+
+if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/vendor/autoload.php';
+require $basePath.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once $basePath.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
